33a4a0dcc92a4085f36ac01ea9806a9a1a37f7dd
Sabine Zschaubitz <mail@sabinezschaubitz.de>
2015-04-04 22:34:04 +0200

    layout

--- application.js
+++ application.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
 
+	$('#geschlossen').hide();
+
+	var fadeInListe = function (e){
+		e.preventDefault();
+		$('#geschlossen').slideToggle();
+	}
+
+	$('.liste-geschlossen').on('click', fadeInListe);
+
 	var clickBtn = function (){
 		var valueAgentur = $('#input-agentur').val();
 		var valueBetrag = $('#input-betrag').val();
 		var valueDatum = $('#input-datum').val();
 
-		$('#noch-offen').prepend( '<tr><td>' + valueDatum + '</td><td>' + valueAgentur + '</td><td>' + valueBetrag + '€' + "</td><td><input type='checkbox'> bezahlt</td></tr>" );
+		$('#noch-offen').prepend( '<tr><td>' + valueDatum + '</td><td>' + valueAgentur + '</td><td>' + valueBetrag + '€' + "</td><td class='bezahlt'><input type='checkbox'> bezahlt</td></tr>" );
 		$('#noch-offen tr:nth-of-type(1)').hide();
 		$('#noch-offen tr:nth-of-type(1)').fadeIn(200);
 
@@ -20,7 +29,14 @@ $(document).ready(function(){
 	var paid = function(){
 		var row = $(this).parents('tr');
 
-		row.fadeOut(200);
+		row.fadeOut(200, function() {
+
+			$(row).appendTo( "#geschlossen" );
+
+			$(row).find('.bezahlt').remove();
+
+			row.fadeIn(200);
+		});
 	}
 
 	$("#noch-offen").on('click', "input[type='checkbox']", paid);
